5358f87d4dce07803fe604190e7f0af029416711
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-24 23:37:54 +0200

    Build App component to use React Router
    And render Navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-function App() {}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import MyProfile from './pages/MyProfile';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="missions" element={<Missions />} />
+      <Route path="profile" element={<MyProfile />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
