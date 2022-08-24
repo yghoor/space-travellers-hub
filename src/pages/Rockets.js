a2400a6243566fc652637d1c17fea8a9494b3505
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-24 23:27:39 +0200

    Create 'Rockets' page

---
+++ src/pages/Rockets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import RocketList from '../components/RocketList';
+
+const Rockets = () => (
+  <div className="rockets-page">
+    <RocketList />
+  </div>
+);
+
+export default Rockets;
