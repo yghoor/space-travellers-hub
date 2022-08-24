9ff9b2090af5686a4867b0fe12435a3951a2f9f3
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-24 23:33:50 +0200

    Create 'MyProfile' page

---
+++ src/pages/MyProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ReservedRockets from '../components/ReservedRockets';
+
+const MyProfile = () => (
+  <div className="profile-page">
+    <h1>My Rockets</h1>
+    <ReservedRockets />
+  </div>
+);
+
+export default MyProfile;
