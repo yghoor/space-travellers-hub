feebbf9273c788c3b558ae8d7332628c0bb465ae
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-25 12:17:40 +0200

    Setup 'App' component to retrieve rockets on load

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { retrieveRockets } from './redux/rockets/rockets';
 
 import Navbar from './components/Navbar';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import MyProfile from './pages/MyProfile';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Rockets />} />
-      <Route path="missions" element={<Missions />} />
-      <Route path="profile" element={<MyProfile />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log('dispatching...');
+    dispatch(retrieveRockets());
+  }, []);
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="profile" element={<MyProfile />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
