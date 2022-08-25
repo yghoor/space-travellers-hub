f82617e1802f81002d27c1bdb6b659441ddf4d51
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-25 12:27:53 +0200

    Setup 'RocketList' to use state to display rockets

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import Rocket from './Rocket';
 
-const RocketList = ({ rocketList }) => (
-  <div className="rocket-list">
-    <ul>
-      {rocketList.map((rocket) => (
-        <Rocket
-          key={rocket.id}
-          imageLink={rocket.flickr_images[0]}
-          name={rocket.rocket_name}
-          description={rocket.description}
-        />
-      ))}
-    </ul>
-  </div>
+const RocketList = () => {
+  const rocketList = useSelector((state) => state.rocketsReducer);
+
+  if (Object.keys(rocketList).length === 0) {
+    return (
+      <div className="loading">
+        <h1>Loading Rockets...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="rocket-list">
+      <ul>
+        {rocketList.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            imageLink={rocket.flickr_images[0]}
+            name={rocket.rocket_name}
+            description={rocket.description}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
